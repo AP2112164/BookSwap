6f6261d9c92e096c71efd9e55a8610a9234c3adb
adam <a.polec@rgu.ac.uk>
2024-04-08 16:26:18 +0100

    button not working, unread as its null (?)

---
+++ app.js
@@ -0,0 +1,37 @@
+/* user will input a book title*/
+const searchBook = document.getElementById('inputBookTitle');
+const btnShowBooks = document.getElementById(`btn-book-searches`);
+btnShowBooks.addEventListener("click", getBooks);
+
+//var img, title, author, genre, description;
+
+
+let bookName = "";
+let bookObject = {};
+
+function getBooks(){
+    
+    const apiURL = `https://www.googleapis.com/books/v1/volumes?q=`;
+    fetch(apiURL).then(getJson).then(updateDisplay).catch(error);
+}
+
+function getJson(aResponse){
+    const theResponse = aResponse.json();
+    console.log(theResponse);
+    return theResponse;
+}
+
+function updateDisplay(jsonObj){
+    let bookNameSearched = searchBook.value;
+    let bookObjArray = jsonObj.results;
+}
+
+// function getJson(aResponse){
+//     const anyResponse = aResponse.json();
+//     console.log(anyResponse);
+//     return anyResponse;
+// }
+
+function anyErrors(error){
+     console.log(error);
+}
---
+++ testData.js
@@ -0,0 +1,322 @@
+{
+    "kind": "books#volume",
+    "id": "1o7D0m_osFEC",
+    "etag": "qomH8UjhTjk",
+    "selfLink": "https://www.googleapis.com/books/v1/volumes/1o7D0m_osFEC",
+    "volumeInfo": {
+        "title": "Harry Potter and the Chamber of Secrets",
+        "authors": [
+            "J.K. Rowling"
+        ],
+        "publisher": "Pottermore Publishing",
+        "publishedDate": "2015-12-08",
+        "description": "'There is a plot, Harry Potter. A plot to make most terrible things happen at Hogwarts School of Witchcraft and Wizardry this year.' Harry Potter's summer has included the worst birthday ever, doomy warnings from a house-elf called Dobby, and rescue from the Dursleys by his friend Ron Weasley in a magical flying car! Back at Hogwarts School of Witchcraft and Wizardry for his second year, Harry hears strange whispers echo through empty corridors - and then the attacks start. Students are found as though turned to stone... Dobby's sinister predictions seem to be coming true. Having become classics of our time, the Harry Potter eBooks never fail to bring comfort and escapism. With their message of hope, belonging and the enduring power of truth and love, the story of the Boy Who Lived continues to delight generations of new readers.",
+        "industryIdentifiers": [
+            {
+                "type": "ISBN_13",
+                "identifier": "9781781100226"
+            },
+            {
+                "type": "ISBN_10",
+                "identifier": "1781100225"
+            }
+        ],
+        "readingModes": {
+            "text": true,
+            "image": true
+        },
+        "pageCount": 350,
+        "printType": "BOOK",
+        "categories": [
+            "Juvenile Fiction"
+        ],
+        "averageRating": 4,
+        "ratingsCount": 29,
+        "maturityRating": "NOT_MATURE",
+        "allowAnonLogging": true,
+        "contentVersion": "4.24.24.0.preview.3",
+        "panelizationSummary": {
+            "containsEpubBubbles": false,
+            "containsImageBubbles": false
+        },
+        "imageLinks": {
+            "smallThumbnail": "http://books.google.com/books/content?id=1o7D0m_osFEC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+            "thumbnail": "http://books.google.com/books/content?id=1o7D0m_osFEC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+        },
+        "language": "en",
+        "previewLink": "http://books.google.co.uk/books?id=1o7D0m_osFEC&printsec=frontcover&dq=harry+potter&hl=&cd=1&source=gbs_api",
+        "infoLink": "https://play.google.com/store/books/details?id=1o7D0m_osFEC&source=gbs_api",
+        "canonicalVolumeLink": "https://play.google.com/store/books/details?id=1o7D0m_osFEC"
+    },
+    "saleInfo": {
+        "country": "GB",
+        "saleability": "FOR_SALE",
+        "isEbook": true,
+        "listPrice": {
+            "amount": 5.99,
+            "currencyCode": "GBP"
+        },
+        "retailPrice": {
+            "amount": 5.99,
+            "currencyCode": "GBP"
+        },
+        "buyLink": "https://play.google.com/store/books/details?id=1o7D0m_osFEC&rdid=book-1o7D0m_osFEC&rdot=1&source=gbs_api",
+        "offers": [
+            {
+                "finskyOfferType": 1,
+                "listPrice": {
+                    "amountInMicros": 5990000,
+                    "currencyCode": "GBP"
+                },
+                "retailPrice": {
+                    "amountInMicros": 5990000,
+                    "currencyCode": "GBP"
+                },
+                "giftable": true
+            }
+        ]
+    },
+    "accessInfo": {
+        "country": "GB",
+        "viewability": "PARTIAL",
+        "embeddable": true,
+        "publicDomain": false,
+        "textToSpeechPermission": "ALLOWED",
+        "epub": {
+            "isAvailable": true,
+            "acsTokenLink": "http://books.google.co.uk/books/download/Harry_Potter_and_the_Chamber_of_Secrets-sample-epub.acsm?id=1o7D0m_osFEC&format=epub&output=acs4_fulfillment_token&dl_type=sample&source=gbs_api"
+        },
+        "pdf": {
+            "isAvailable": true,
+            "acsTokenLink": "http://books.google.co.uk/books/download/Harry_Potter_and_the_Chamber_of_Secrets-sample-pdf.acsm?id=1o7D0m_osFEC&format=pdf&output=acs4_fulfillment_token&dl_type=sample&source=gbs_api"
+        },
+        "webReaderLink": "http://play.google.com/books/reader?id=1o7D0m_osFEC&hl=&source=gbs_api",
+        "accessViewStatus": "SAMPLE",
+        "quoteSharingAllowed": false
+    },
+    "searchInfo": {
+        "textSnippet": "With their message of hope, belonging and the enduring power of truth and love, the story of the Boy Who Lived continues to delight generations of new readers."
+    }
+},
+{
+    "kind": "books#volume",
+    "id": "kLAoswEACAAJ",
+    "etag": "kDQbq14hE98",
+    "selfLink": "https://www.googleapis.com/books/v1/volumes/kLAoswEACAAJ",
+    "volumeInfo": {
+        "title": "Harry Potter and the Cursed Child",
+        "subtitle": "Parts One and Two Playscript",
+        "authors": [
+            "J. K. Rowling",
+            "Jack Thorne",
+            "John Tiffany"
+        ],
+        "publishedDate": "2017",
+        "description": "As an overworked employee of the Ministry of Magic, a husband, and a father, Harry Potter struggles with a past that refuses to stay where it belongs while his youngest son, Albus, finds the weight of the family legacy difficult to bear.",
+        "industryIdentifiers": [
+            {
+                "type": "ISBN_10",
+                "identifier": "0751565369"
+            },
+            {
+                "type": "ISBN_13",
+                "identifier": "9780751565362"
+            }
+        ],
+        "readingModes": {
+            "text": false,
+            "image": false
+        },
+        "pageCount": 0,
+        "printType": "BOOK",
+        "categories": [
+            "Fiction"
+        ],
+        "averageRating": 3,
+        "ratingsCount": 6,
+        "maturityRating": "NOT_MATURE",
+        "allowAnonLogging": false,
+        "contentVersion": "preview-1.0.0",
+        "panelizationSummary": {
+            "containsEpubBubbles": false,
+            "containsImageBubbles": false
+        },
+        "imageLinks": {
+            "smallThumbnail": "http://books.google.com/books/content?id=kLAoswEACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api",
+            "thumbnail": "http://books.google.com/books/content?id=kLAoswEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
+        },
+        "language": "en",
+        "previewLink": "http://books.google.co.uk/books?id=kLAoswEACAAJ&dq=harry+potter&hl=&cd=2&source=gbs_api",
+        "infoLink": "http://books.google.co.uk/books?id=kLAoswEACAAJ&dq=harry+potter&hl=&source=gbs_api",
+        "canonicalVolumeLink": "https://books.google.com/books/about/Harry_Potter_and_the_Cursed_Child.html?hl=&id=kLAoswEACAAJ"
+    },
+    "saleInfo": {
+        "country": "GB",
+        "saleability": "NOT_FOR_SALE",
+        "isEbook": false
+    },
+    "accessInfo": {
+        "country": "GB",
+        "viewability": "NO_PAGES",
+        "embeddable": false,
+        "publicDomain": false,
+        "textToSpeechPermission": "ALLOWED",
+        "epub": {
+            "isAvailable": false
+        },
+        "pdf": {
+            "isAvailable": false
+        },
+        "webReaderLink": "http://play.google.com/books/reader?id=kLAoswEACAAJ&hl=&source=gbs_api",
+        "accessViewStatus": "NONE",
+        "quoteSharingAllowed": false
+    },
+    "searchInfo": {
+        "textSnippet": "As an overworked employee of the Ministry of Magic, a husband, and a father, Harry Potter struggles with a past that refuses to stay where it belongs while his youngest son, Albus, finds the weight of the family legacy difficult to bear."
+    }
+},
+{
+    "kind": "books#volume",
+    "id": "3ZnHBAAAQBAJ",
+    "etag": "xsx6b5ckMH0",
+    "selfLink": "https://www.googleapis.com/books/v1/volumes/3ZnHBAAAQBAJ",
+    "volumeInfo": {
+        "title": "Harry Potter and the Chamber of Secrets",
+        "authors": [
+            "J. K. Rowling"
+        ],
+        "publisher": "Bloomsbury Publishing",
+        "publishedDate": "2014-08-27",
+        "description": "When the Chamber of Secrets is opened again at the Hogswart School for Witchcraft and Wizardry, second-year student Harry Potter finds himself in danger from a dark power that has once more been released on the school.",
+        "industryIdentifiers": [
+            {
+                "type": "ISBN_13",
+                "identifier": "9781408855904"
+            },
+            {
+                "type": "ISBN_10",
+                "identifier": "1408855909"
+            }
+        ],
+        "readingModes": {
+            "text": false,
+            "image": true
+        },
+        "pageCount": 373,
+        "printType": "BOOK",
+        "categories": [
+            "Juvenile Fiction"
+        ],
+        "averageRating": 5,
+        "ratingsCount": 1,
+        "maturityRating": "NOT_MATURE",
+        "allowAnonLogging": false,
+        "contentVersion": "1.2.3.0.preview.1",
+        "panelizationSummary": {
+            "containsEpubBubbles": false,
+            "containsImageBubbles": false
+        },
+        "imageLinks": {
+            "smallThumbnail": "http://books.google.com/books/content?id=3ZnHBAAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+            "thumbnail": "http://books.google.com/books/content?id=3ZnHBAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+        },
+        "language": "en",
+        "previewLink": "http://books.google.co.uk/books?id=3ZnHBAAAQBAJ&printsec=frontcover&dq=harry+potter&hl=&cd=3&source=gbs_api",
+        "infoLink": "http://books.google.co.uk/books?id=3ZnHBAAAQBAJ&dq=harry+potter&hl=&source=gbs_api",
+        "canonicalVolumeLink": "https://books.google.com/books/about/Harry_Potter_and_the_Chamber_of_Secrets.html?hl=&id=3ZnHBAAAQBAJ"
+    },
+    "saleInfo": {
+        "country": "GB",
+        "saleability": "NOT_FOR_SALE",
+        "isEbook": false
+    },
+    "accessInfo": {
+        "country": "GB",
+        "viewability": "PARTIAL",
+        "embeddable": true,
+        "publicDomain": false,
+        "textToSpeechPermission": "ALLOWED",
+        "epub": {
+            "isAvailable": false
+        },
+        "pdf": {
+            "isAvailable": true,
+            "acsTokenLink": "http://books.google.co.uk/books/download/Harry_Potter_and_the_Chamber_of_Secrets-sample-pdf.acsm?id=3ZnHBAAAQBAJ&format=pdf&output=acs4_fulfillment_token&dl_type=sample&source=gbs_api"
+        },
+        "webReaderLink": "http://play.google.com/books/reader?id=3ZnHBAAAQBAJ&hl=&source=gbs_api",
+        "accessViewStatus": "SAMPLE",
+        "quoteSharingAllowed": false
+    },
+    "searchInfo": {
+        "textSnippet": "When the Chamber of Secrets is opened again at the Hogswart School for Witchcraft and Wizardry, second-year student Harry Potter finds himself in danger from a dark power that has once more been released on the school."
+    }
+},
+{
+    "kind": "books#volume",
+    "id": "xNgstAEACAAJ",
+    "etag": "IkM3xsuOVFU",
+    "selfLink": "https://www.googleapis.com/books/v1/volumes/xNgstAEACAAJ",
+    "volumeInfo": {
+        "title": "Fantastic Beasts and Where to Find Them",
+        "authors": [
+            "J K. ROFLING"
+        ],
+        "publisher": "Blurb",
+        "publishedDate": "2017-09-02",
+        "description": "\"Fantastic Beasts and Where to Find Them: Coloring Book\" is parody of \"Fantastic Beasts and Where to Find Them: Magical Creatures Coloring Book.\" Harry Potter fans are loving coloring this book.",
+        "industryIdentifiers": [
+            {
+                "type": "ISBN_10",
+                "identifier": "1389679071"
+            },
+            {
+                "type": "ISBN_13",
+                "identifier": "9781389679070"
+            }
+        ],
+        "readingModes": {
+            "text": false,
+            "image": false
+        },
+        "printType": "BOOK",
+        "maturityRating": "NOT_MATURE",
+        "allowAnonLogging": false,
+        "contentVersion": "preview-1.0.0",
+        "panelizationSummary": {
+            "containsEpubBubbles": false,
+            "containsImageBubbles": false
+        },
+        "imageLinks": {
+            "smallThumbnail": "http://books.google.com/books/content?id=xNgstAEACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api",
+            "thumbnail": "http://books.google.com/books/content?id=xNgstAEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
+        },
+        "language": "en",
+        "previewLink": "http://books.google.co.uk/books?id=xNgstAEACAAJ&dq=harry+potter&hl=&cd=4&source=gbs_api",
+        "infoLink": "http://books.google.co.uk/books?id=xNgstAEACAAJ&dq=harry+potter&hl=&source=gbs_api",
+        "canonicalVolumeLink": "https://books.google.com/books/about/Fantastic_Beasts_and_Where_to_Find_Them.html?hl=&id=xNgstAEACAAJ"
+    },
+    "saleInfo": {
+        "country": "GB",
+        "saleability": "NOT_FOR_SALE",
+        "isEbook": false
+    },
+    "accessInfo": {
+        "country": "GB",
+        "viewability": "NO_PAGES",
+        "embeddable": false,
+        "publicDomain": false,
+        "textToSpeechPermission": "ALLOWED",
+        "epub": {
+            "isAvailable": false
+        },
+        "pdf": {
+            "isAvailable": false
+        },
+        "webReaderLink": "http://play.google.com/books/reader?id=xNgstAEACAAJ&hl=&source=gbs_api",
+        "accessViewStatus": "NONE",
+        "quoteSharingAllowed": false
+    },
+    "searchInfo": {
+        "textSnippet": "&quot;Fantastic Beasts and Where to Find Them: Coloring Book&quot; is parody of &quot;Fantastic Beasts and Where to Find Them: Magical Creatures Coloring Book.&quot; Harry Potter fans are loving coloring this book."
+    }
+}
