acbcd798b8b827efd67f16dedc8e9b8b84d55bc9
adam <a.polec@rgu.ac.uk>
2024-04-09 08:50:42 +0100

    button still not functioning

--- app.js
+++ app.js
@@ -1,10 +1,46 @@
 /* user will input a book title*/
 const searchBook = document.getElementById("inputBookTitle");
 const btnShowBooks = document.getElementById("btn-book-searches");
-btnShowBooks.addEventListener("click", getBooks);
+btnShowBooks.addEventListener('click', getBooks);
 
 //var img, title, author, genre, description;
 
+function getBooks(){
+    const bookTerm = searchBook.value;
+    console.log("BUTTON PRESSED");
+
+    getResults();
+}
+
+async function getResults(){
+    const apiURL = `https://www.googleapis.com/books/v1/volumes?q=${bookTerm}`;
+    
+    try {
+        const response = await fetch(apiURL);
+        const json = await getJson(response);
+        updateDisplay(json);
+    } catch (error) {
+        reportError(error);
+    }
+}
+
+async function getJson(aResponse){
+    return aResponse.json();
+}
+
+async function updateDisplay(jsonObj){
+
+    try {
+        const data = jsonObj;
+        //console.log(jsonObj);
+    } catch (error) {
+        reportError(error);
+    }
+}
+
+async function reportError(anError){
+    console.log(anError);
+}
 
 // function getBooks(){
     
