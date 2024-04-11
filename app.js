1b569e7a05bc496a6f56b322a8d4c8273fe39524
adam <a.polec@rgu.ac.uk>
2024-04-11 09:43:46 +0100

    further comments were added

--- app.js
+++ app.js
@@ -21,10 +21,12 @@ const bSaveToFavourites = document.getElementById('saveBook');
 let favBookCount;
 let booksArray = {};
 
+//buttons are not working due to addEventListener issue
 bSaveToSession.addEventListener('click', saveInputToSession);
 bBookResults.addEventListener('click', getBooks);
 bSaveToFavourites.addEventListener('click', saveBookToShelf)
 
+// API being used is the Open Library API
 const apiAddress = `https://openlibrary.org/search.json?q=`;
 
 function saveInputs(){/* this should save the input from the user and rember it for them */
@@ -73,7 +75,7 @@ function buildResults(jsonObj){
 customElements.define('book-results', BookResults);
 const nav = document.querySelector('ion-nav');
 
-// should lead user to new page 
+// should lead user to new page, that shows details about an individual book 
  function showBookDetails(aTitle){
     
     let book = new Object;
@@ -94,7 +96,7 @@ function anyErrors(){/* will report any errors when fetching the API */
 
 async function saveBookToShelf(aBook){// should build a new item in shelf page when a button is clicked
 
-    class BookResults extends HTMLElement{
+    class bookToShelf extends HTMLElement{
         
         connectedCallback(){
              this.innerHTML = `
